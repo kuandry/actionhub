92a7edaeb5f1e369911c3e834e8228a47e0b056f
kuandry <cariocahdouglas@gmail.com>
2026-05-08 22:21:27 -0300

    Criação templates base, remove arquivos sensíveis e adiciona .gitignore

---
+++ init_db.py
@@ -0,0 +1,7 @@
+from app import app
+from core.extensions import db
+
+with app.app_context():
+    db.create_all()
+    print("✅ Banco de dados criado com sucesso!")
+    print("📁 Localização: instance/database.db")
